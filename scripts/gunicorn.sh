ac55a7552119e0682ca14a8f04083779dd2f69a9
piyush15081999 <157483801+piyush15081999@users.noreply.github.com>
2024-04-09 16:55:37 +0530

    Update gunicorn.sh

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-sudo systemctl daemon-reload
+
 sudo cp /home/ubuntu/bus_resv/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
 sudo cp /home/ubuntu/bus_resv/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
 
@@ -8,3 +8,4 @@ sudo systemctl enable gunicorn.service
 
 sudo systemctl start gunicorn.socket
 sudo systemctl enable gunicorn.socket
+sudo systemctl daemon-reload
